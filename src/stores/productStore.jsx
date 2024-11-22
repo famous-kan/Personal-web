5f93f1d1a6bcd057e13d7420d01b66bf3192c6c5
famous-kan <Kanyakorn_fame@hotmail.com>
2024-11-22 13:25:32 +0700

    Deploy

--- src/stores/productStore.jsx
+++ src/stores/productStore.jsx
@@ -5,17 +5,20 @@ import axios from "axios";
 const useProductStore = create((set,get) => ({
     products: [],
     loading : false,
+    URL : import.meta.env.VITE_API_URL,
 
     getAllProducts : async(count = 9,page =1,token) => {
         set({loading: true})
-        const result = await axios.get(`http://localhost:8000/product/${count}/${page}`, {
+        const {URL} = get()
+        const result = await axios.get(`${URL}/product/${count}/${page}`, {
             headers : {Authorization : `Bearer ${token}`}
         })
         console.log(result)
         set({ products: result.data , loading: false})
         },
     searchFilter : async (arg) => {
-        const result = await axios.post('http://localhost:8000/product/search/filters', arg)
+        const {URL} = get()
+        const result = await axios.post(`${URL}/product/search/filters`, arg)
         console.log(result)
         set({products : result.data})
         
